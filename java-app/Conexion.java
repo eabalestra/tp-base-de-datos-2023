449075088514f60c8589b46b1d4669900256f02e
matybq <matybq20@gmail.com>
2023-06-01 19:50:59 -0300

    connection

--- java-app/Conexion.java
+++ java-app/Conexion.java
@@ -1,21 +1,34 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
-/**
- * 
- */
-public class Conexion {
-  private static String url = "jdbc:postgresql://localhost/test";
-  private static String username = "postgres";
-  private static String password = "root";
-  private static Connection connection;
 
-  public static Connection getInstance() throws SQLException{
-    if (connection == null){
-      connection = DriverManager.getConnection(url, username, password);
+
+ public class Conexion {
+
+  public static void main(String[] args) {
+
+    String driver = "org.postgresql.Driver";
+    String url = "jdbc:postgresql://localhost:5432/maneja_seguro";
+    String username = "postgres";
+    String password = "123456";
+
+    try {
+
+      Class.forName(driver);
+
+      Connection connection = DriverManager.getConnection(url, username, password);
+
+      String query = "SELECT * FROM cliente ";
+      PreparedStatement statement = connection.prepareStatement(query);
+      ResultSet resultSet = statement.executeQuery();
+
+      while (resultSet.next()) {
+        System.out.println(resultSet.getInt("id"));
+      }
+
+    } 
+    catch (SQLException e) {
+      System.out.println("Error: "+ e);;
     }
-    return connection;
   }
-  
+
 }
